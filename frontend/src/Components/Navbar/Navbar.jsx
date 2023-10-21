345a7d94bfad814da1e771c25c68bea390e495c1
ernkeyz <exconzy26@gmail.com>
2023-10-21 21:57:27 +0000

    navbar_state-management

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,8 @@ import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
   return (
     <div className="navbar">
       <div className="nav-logo">
