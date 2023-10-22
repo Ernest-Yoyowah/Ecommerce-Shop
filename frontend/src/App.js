79dc0c8e9aaa8a01098214d506f4260f2ee545ef
ernkeyz <exconzy26@gmail.com>
2023-10-22 20:24:21 +0000

    Pages: Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Pages/Shop";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
