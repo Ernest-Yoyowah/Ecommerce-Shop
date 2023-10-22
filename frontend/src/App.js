2a1be63fae84cc2bfad35a6f95ab840add7a55ef
ernkeyz <exconzy26@gmail.com>
2023-10-22 20:28:34 +0000

    Pages: Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory />} />
+          <Route path="/women" element={<ShopCategory />} />
+          <Route path="/kids" element={<ShopCategory />} />
         </Routes>
       </BrowserRouter>
     </>
