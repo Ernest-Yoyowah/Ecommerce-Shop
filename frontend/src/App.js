b8b4c52d00dc036069a50a158d63a384df02f14a
ernkeyz <exconzy26@gmail.com>
2023-10-22 20:39:20 +0000

    Pages: Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Shop from "./Pages/Shop";
+import Cart from "./Pages/Cart";
 import ShopCategory from "./Pages/ShopCategory";
+import LoginSignup from "./Pages/LoginSignup";
 
 import Product from "./Pages/Product";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -16,7 +18,12 @@ function App() {
           <Route path="/men" element={<ShopCategory category="men" />} />
           <Route path="/women" element={<ShopCategory category="women" />} />
           <Route path="/kids" element={<ShopCategory category="kids" />} />
-          <Route path="/product" element={<Product />}></Route>
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
       </BrowserRouter>
     </>
