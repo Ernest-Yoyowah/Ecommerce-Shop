3edb45743eafad3ab8bba7ceedfe80fe29bca5ee
ernkeyz <exconzy26@gmail.com>
2023-10-25 20:53:27 +0000

    Hero Section left side

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "./Hero.css";
+import hand_icon from "../Assets/hand_icon.png";
+
 const Hero = () => {
-  return <div className="hero">Hero</div>;
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <h2>New Arrivals Only</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="hand icon" />
+          </div>
+        </div>
+      </div>
+      <div className="hero-rigth"></div>
+    </div>
+  );
 };
 
 export default Hero;
