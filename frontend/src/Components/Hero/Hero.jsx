5491be9880cf6d71d89ef57cb8737505a7e03864
ernkeyz <exconzy26@gmail.com>
2023-10-25 20:54:07 +0000

    Hero Section left side

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>New Arrivals Only</h2>
+        <h2>NEW ARRIVALS ONLY</h2>
         <div>
           <div className="hero-hand-icon">
             <p>new</p>
