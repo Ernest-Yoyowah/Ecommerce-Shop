8cb742f846edc29c7d10caf2566617a3f0130779
ernkeyz <exconzy26@gmail.com>
2023-10-25 20:48:02 +0000

    Hero Section Route

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "../Components/Hero/Hero";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Shop;
