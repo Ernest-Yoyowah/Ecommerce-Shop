682d6e379398830a113b2df71a38566b61170bc2
ernkeyz <exconzy26@gmail.com>
2023-10-25 20:59:09 +0000

    Hero Section latest collection

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Hero.css";
 import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
 
 const Hero = () => {
   return (
@@ -12,6 +13,12 @@ const Hero = () => {
             <p>new</p>
             <img src={hand_icon} alt="hand icon" />
           </div>
+          <p>collections</p>
+          <p>for everyone</p>
+        </div>
+        <div className="hero-left-btn">
+          <div>Latest Collection</div>
+          <img src={arrow_icon} alt="arrow icon" />
         </div>
       </div>
       <div className="hero-rigth"></div>
